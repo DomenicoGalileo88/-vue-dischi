6d9c08102744273ee1bbec25e0fd79be3df3ef77
DomenicoGalileo88 <domenico.galileo@gmail.com>
2022-05-05 15:44:40 +0200

    initial commit and header

---
+++ spotify-dischi/src/components/HeaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+      <div class="logo">
+      <img width="40" src="@/assets/img/logo-small.svg" alt="Spotify logo">
+  </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+};
+</script>
+
+<style lang="scss" scoped>
+    header{
+        background-color: rgb(12, 118, 153);
+        padding: 0.7rem;
+        img{
+    width: 40px;
+}
+    }
+</style>
